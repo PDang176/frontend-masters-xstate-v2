784554ab97172a2895a70a4619c175dfd97efc49
Dang, Patrick T <patrick.dang@pnnl.gov>
2022-11-18 12:03:10 -0800

    LESSON 01: State Modeling Exercise

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -6,3 +6,35 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+const machine = {
+  initialState: 'loading',
+  states: {
+    loading: {
+      on: { LOADED: 'playing' }
+    },
+    playing: {
+      on: { PAUSE: 'paused' }
+    },
+    paused: {
+      on: { PLAY: 'playing' }
+    }
+  }
+}
+
+/**
+ * 
+ * @param {status: string} state 
+ * @param {type: string} event 
+ * @returns 
+ */
+function transition(state, event) {
+  const newStatus = machine.states[state.status]?.on?.[event.type] ?? state.status;
+
+  return {
+    status: newStatus
+  };
+}
+
+window.machine = machine;
+window.transition = transition;
