ea6e0977ba26b6b3925b75bcb863c188598c0599
Dang, Patrick T <patrick.dang@pnnl.gov>
2022-11-18 11:38:43 -0800

    LESSON 01: Software Modeling and State Machine in Vanilla Javascript

--- main.js
+++ main.js
@@ -2,3 +2,47 @@ import './style.css';
 import { createMachine } from 'xstate';
 
 console.log("Hello World!");
+
+// state: { data: null, error: null }
+// event: { type: 'FETCH' }
+function transition(state, event){
+  switch(state.status){
+    case 'idle':
+      if(event.type === 'FETCH'){
+        console.log('Starting to fetch data');
+        return { status: 'loading' };
+      }
+      return state;
+    case 'loading':
+      // other behavior
+      break;
+    default:
+      break;
+  }
+
+  return state;
+}
+
+const machine = {
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        FETCH: 'loading'
+      }
+    },
+    loading: {}
+  }
+}
+
+function transition2(state, event){
+  const nextStatus = machine.states[state.status].on?.[event.type] ?? state.status;
+
+  return {
+    status: nextStatus
+  };
+}
+
+window.machine = machine;
+window.transition = transition;
+window.transition2 = transition2;
