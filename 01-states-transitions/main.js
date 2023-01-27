a53fc8f20a35e1f3d4d1dd0761b1c7883e1b3285
Dang, Patrick T <patrick.dang@pnnl.gov>
2023-01-27 09:48:15 -0800

    LESSON 01: State Transitions

--- 01-states-transitions/main.js
+++ 01-states-transitions/main.js
@@ -1,8 +1,10 @@
 // @ts-check
 import '../style.css';
+// @ts-ignore
 import { createMachine, assign, interpret, send } from 'xstate';
 import elements from '../utils/elements';
 
+// @ts-ignore
 import { inspect } from '@xstate/inspect';
 
 // inspect({
@@ -10,21 +12,45 @@ import { inspect } from '@xstate/inspect';
 //   url: 'https://stately.ai/viz?inspect',
 // });
 
-const playerMachine = createMachine({});
+const playerMachine = createMachine({
+  initial: 'loading',
+  states: {
+    loading: {
+      on: {
+        LOADED: { target: 'playing' },
+      },
+    },
+    paused: {
+      on: {
+        PLAY: { target: 'playing' },
+      },
+    },
+    playing: {
+      on: {
+        PAUSE: { target: 'paused' },
+      },
+    },
+  },
+});
 
 const service = interpret(playerMachine, { devTools: true }).start();
 
+// @ts-ignore
 elements.elPlayButton.addEventListener('click', () => {
   service.send({ type: 'PLAY' });
 });
+// @ts-ignore
 elements.elPauseButton.addEventListener('click', () => {
   service.send({ type: 'PAUSE' });
 });
 
 service.subscribe((state) => {
   console.log(state);
+  // @ts-ignore
   elements.elLoadingButton.hidden = !state.matches('loading');
+  // @ts-ignore
   elements.elPlayButton.hidden = !state.can({ type: 'PLAY' });
+  // @ts-ignore
   elements.elPauseButton.hidden = !state.can({ type: 'PAUSE' });
 });
 
--- main.js
+++ main.js
@@ -1,48 +1,79 @@
 import './style.css';
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from 'xstate';
 
-console.log("Hello World!");
-
-// state: { data: null, error: null }
-// event: { type: 'FETCH' }
-function transition(state, event){
-  switch(state.status){
-    case 'idle':
-      if(event.type === 'FETCH'){
-        console.log('Starting to fetch data');
-        return { status: 'loading' };
-      }
-      return state;
-    case 'loading':
-      // other behavior
-      break;
-    default:
-      break;
-  }
-
-  return state;
-}
-
-const machine = {
-  initial: 'idle',
+const machine = createMachine({
+  initial: 'loading',
   states: {
-    idle: {
+    loading: {
       on: {
-        FETCH: 'loading'
-      }
+        SUCCESS: 'loaded',
+      },
     },
-    loading: {}
+    loaded: {},
   }
-}
+});
 
-function transition2(state, event){
-  const nextStatus = machine.states[state.status].on?.[event.type] ?? state.status;
+// console.log(machine.initialState);
 
-  return {
-    status: nextStatus
-  };
-}
+// const nextState = machine.transition(undefined, {
+//   type: 'SUCCESS'
+// });
 
-window.machine = machine;
-window.transition = transition;
-window.transition2 = transition2;
+// console.log(nextState);
+
+// console.log(nextState.matches('loaded'));
+
+const service = interpret(machine).start();
+
+service.subscribe((state) => {
+  console.log(state.value);
+});
+
+window.service = service;
+
+// Lesson 00
+// console.log("Hello World!");
+
+// // state: { data: null, error: null }
+// // event: { type: 'FETCH' }
+// function transition(state, event){
+//   switch(state.status){
+//     case 'idle':
+//       if(event.type === 'FETCH'){
+//         console.log('Starting to fetch data');
+//         return { status: 'loading' };
+//       }
+//       return state;
+//     case 'loading':
+//       // other behavior
+//       break;
+//     default:
+//       break;
+//   }
+
+//   return state;
+// }
+
+// const machine = {
+//   initial: 'idle',
+//   states: {
+//     idle: {
+//       on: {
+//         FETCH: 'loading'
+//       }
+//     },
+//     loading: {}
+//   }
+// }
+
+// function transition2(state, event){
+//   const nextStatus = machine.states[state.status].on?.[event.type] ?? state.status;
+
+//   return {
+//     status: nextStatus
+//   };
+// }
+
+// window.machine = machine;
+// window.transition = transition;
+// window.transition2 = transition2;
